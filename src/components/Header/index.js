42809518d255e57ff2efb23a9694b315c0c43313
Mon <rocketorangemen@gmail.com>
2021-06-12 12:46:29 -0500

    components progress

---
+++ src/components/Header/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../Portfolio.css';
+
+function Header () {
+    return(
+    <header>
+      <div class="nav-container">
+      <h1>Ramon Flowers Portfolio</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#about">About Me</a>
+          </li>
+          <li>
+            <a href="#work-section">Work</a>
+          </li>
+          <li>
+            <a href="#contact">Contact Info</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    </header>
+    )}
+
+    export default Header;
---
+++ src/components/Hero/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import '../Portfolio.css';
+
+function Hero () {
+    return(
+
+
+        <div>
+
+    <section id="hero">
+      <h2>Nothing worth having comes easy...</h2>
+    </section>
+    <section id="about">
+      <div class="about-me-box">
+        <div class="title">
+          <h2 class="titles">About Me</h2>
+        </div>
+        <div class="text">
+          <p>
+            My name is Ramon Flowers and I am an aspiring software developer who is looking forward to showcasing my skills and experience with the technology industry.
+          </p>
+          <br/>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+            praesentium animi consequatur iusto facere laudantium nesciunt enim
+            at ipsum. Modi recusandae esse quisquam amet nostrum corrupti ex
+            eligendi rerum officiis. Lorem ipsum dolor, sit amet consectetur
+            adipisicing elit. Ipsam soluta facilis, ea temporibus, ducimus amet
+            impedit adipisci velit nam, at libero dolores! Atque nulla quas
+            explicabo veritatis repellendus minima nemo!
+          </p>
+        </div>
+      </div>
+    </section>
+    </div>
+    );
+}
+
+export default Hero;
