5e561fab8a63247d4cc701dea29abab1e6ec3941
Mon <rocketorangemen@gmail.com>
2021-06-12 11:58:13 -0500

    body issues resolved

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -2,52 +2,59 @@ import React from 'react';
 
 function Body () {
     return (
-<body id="work-section">
+<div class="grid-container">
+    <div class="grid-header">
+      <h2>Work</h2>
+    </div>
+        <div class="main-grid">
+  <div class="work-item work-item-1">
     <div>
-            <div class="grid-container">
-                <div class="grid-header">
-                <h2>Work</h2>
-                </div>
-                    <div class="main-grid">
-            <div class="work-item work-item-1">
-                <div>
-                <h3>iCamp</h3>
-                <p>Front-End Project</p>
-            </div>
-                <a href="https://dragoonkite.github.io/iCamp/"><img src="./assets/images/iCamp.png" alt="icamp-Image"></a>
-            </div>
-            <div class="work-item work-item-2">
-                <div>
-                <h3>Password-Generator</h3>
-                <p>Javascript</p>
-            </div>
-            <a href="https://github.com/rocketorangemen/Password-Generator"><img src="./assets/images/generator.png" alt="Password-Generator"></a>
-            </div>
-            <div class="work-item work-item-3">
-                <div>
-                <h3>weather-Dashboard</h3>
-                <p>Server-side API calls</p>
-            </div>
-            <a href="https://rocketorangemen.github.io/weather-dashboard/"><img src="./assets/images/weather.png" alt="weather-dashboard"></a>
-            </div>
-            <div class="work-item work-item-4">
-                <div>
-                <h3>Horiseon</h3>
-                <p>Refactoring HTML</p>
-            </div>
-            <a href="https://github.com/DragoonKite/eat-out-austin"><img src="./assets/images/screenshot.png" alt="Eat-Out-Austin"></a>
-            </div>
-            <div class="work-item work-item-5">
-                <div>
-                <h3>RUN Buddy</h3>
-                <p>HTML/CSS</p>
-            </div>
-            <a href="https://github.com/rocketorangemen/run-buddy"><img src="./assets/images/item-55.jpg" alt="Run Buddy Website"></a>
-            </div>
-            </div>
-            </div>
-        </div>
-</body>
+    <h3>iCamp</h3>
+    <p>Front-End Project</p>
+  </div>
+    <a href="https://dragoonkite.github.io/iCamp/">
+        <img src="./assets/images/iCamp.png" alt="icamp">
+            </img>
+        </a>
+  </div>
+  <div class="work-item work-item-2">
+    <div>
+    <h3>Password-Generator</h3>
+    <p>Javascript</p>
+  </div>
+  <a href="https://github.com/rocketorangemen/Password-Generator">
+      <img src="./assets/images/generator.png" alt="Password-Generator">
+          </img>
+      </a>
+  </div>
+  <div class="work-item work-item-3">
+    <div>
+    <h3>weather-Dashboard</h3>
+    <p>Server-side API calls</p>
+  </div>
+  <a href="https://rocketorangemen.github.io/weather-dashboard/">
+      <img src="./assets/images/weather.png" alt="weather-dashboard">
+          </img>
+      </a>
+  </div>
+  <div class="work-item work-item-4">
+    <div>
+    <h3>Horiseon</h3>
+    <p>Refactoring HTML</p>
+  </div>
+  <a href="https://github.com/DragoonKite/eat-out-austin"><img src="./assets/images/screenshot.png" alt="Eat-Out-Austin"></img></a>
+  </div>
+  <div class="work-item work-item-5">
+    <div>
+    <h3>RUN Buddy</h3>
+    <p>HTML/CSS</p>
+  </div>
+  <a href="https://github.com/rocketorangemen/run-buddy"><img src="./assets/images/item-55.jpg" alt="Run Buddy Website"></img></a>
+  </div>
+  </div>
+  </div>
 
 );
 }
+
+export default Body;
